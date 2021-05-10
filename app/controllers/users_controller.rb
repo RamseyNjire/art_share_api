5711120ed6dc16e14253af56f1b3e8a3038f8289
Ramsey Njire <ramseynjire@gmail.com>
2021-05-10 22:36:24 +0300

    add controller methods to handle restful routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,42 @@
 class UsersController < ApplicationController
+    def index
+        users = User.all
+
+        render json: users
+    end
+
+    def create
+        user = User.new(user_params)
+
+        if user.save
+            render json: user
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def show
+        render json: User.find(params[:id])
+    end
+
+    def update
+        user = User.find(params[:id])
+
+        if user.update_attributes(user_params)
+            render json: user
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        user = User.find(params[:id])
+        user.destroy
+        render json: user
+    end
+    
+    private
+
     def user_params
         params.require(:user).permit(:username)
     end
