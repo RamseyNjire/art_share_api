8260cab746422b7338f8e73c3e6bb3c71fc716d7
Ramsey Njire <ramseynjire@gmail.com>
2021-05-10 21:50:33 +0300

    generate migration to ensure same artwork cannot be shared with a user more than once

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_10_182900) do
+ActiveRecord::Schema.define(version: 2021_05_10_184847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_05_10_182900) do
     t.integer "viewer_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["artwork_id", "viewer_id"], name: "index_artwork_shares_on_artwork_id_and_viewer_id", unique: true
   end
 
   create_table "artworks", force: :cascade do |t|
