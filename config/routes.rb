9068a8b6f9320fc1b6e63bf82abab7e9760c7d2b
Ramsey Njire <ramseynjire@gmail.com>
2021-05-12 22:17:57 +0300

    create controller and routes for artworks

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :create, :show, :update, :destroy]
+  resources :artworks, only: [:index, :create, :show, :update, :destroy]
 end
