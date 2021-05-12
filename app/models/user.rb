fde611083e9c94abdff580fc4252b22feb9250a0
Ramsey Njire <ramseynjire@gmail.com>
2021-05-12 21:56:42 +0300

    add dependet#destroy to user associations to avoid orphaned records

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,16 @@ class User < ApplicationRecord
         :artworks,
         class_name: 'Artwork',
         foreign_key: :artist_id,
-        primary_key: :id
+        primary_key: :id,
+        dependent: :destroy
     )
 
     has_many(
         :artwork_shares,
         class_name: 'ArtworkShare',
         foreign_key: :viewer_id,
-        primary_key: :id
+        primary_key: :id,
+        dependent: :destroy
     )
 
     has_many(
