56dbe8566dc1797bc748e22d4db430bfb10134f9
Ramsey Njire <ramseynjire@gmail.com>
2021-05-13 23:45:54 +0300

    create controller actions and routes for artwork shares

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :create, :show, :update, :destroy]
   resources :artworks, only: [:index, :create, :show, :update, :destroy]
+  resources :artwork_shares, only: [:create, :destroy]
 end
