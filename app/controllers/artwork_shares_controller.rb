0e7abefc440743e95f4bd9e561576d00515dbe90
Ramsey Njire <ramseynjire@gmail.com>
2021-05-13 23:35:21 +0300

    write private artwork_share_params method

--- app/controllers/artwork_shares_controller.rb
+++ app/controllers/artwork_shares_controller.rb
@@ -1,2 +1,8 @@
 class ArtworkSharesController < ApplicationController
+    
+    private
+
+    def artwork_share_params
+        params.require(:artwork_share).permit(:artwork_id, :viewer_id)
+    end
 end
