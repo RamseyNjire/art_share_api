36648208716d31c5684f89388c71a60a83bed6af
Ramsey Njire <ramseynjire@gmail.com>
2021-05-16 21:21:49 +0300

    build comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,33 @@
 class CommentsController < ApplicationController
+    def index
+        if params.has_key?(:user_id)
+            comments = Comment.where(commenter_id: params[:user_id])
+        elsif params.has_key?(:artwork_id)
+            comments = Comment.where(artwork_id: params[:artwork_id])
+        end
+
+        render json: comments
+    end
+
+    def create
+        comment = Comment.new(comment_params)
+
+        if comment.save
+            render json: comment
+        else
+            render json: comment.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        comment = Comment.find(params[:id])
+        comment.destroy
+        render json: comment
+    end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:artwork_id, :commenter_id, :body)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :create, :show, :update, :destroy]
   resources :users do
     resources :artworks, only: :index
+    resources :comments, only: [:index]
   end
   resources :artworks, only: [:create, :show, :update, :destroy]
+  resources :artworks do
+    resources :comments, only: [:index]
+  end
   resources :artwork_shares, only: [:create, :destroy]
-  resources :comments, only: [:index, :create, :destroy]
+  resources :comments, only: [:create, :destroy]
 end
