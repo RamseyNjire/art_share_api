3b1618d9de4d9182f8f54cd897c8692dea3c270e
Ramsey Njire <ramseynjire@gmail.com>
2021-05-17 22:34:05 +0300

    create migration to add user reference to likes table

---
+++ db/migrate/20210517192816_add_user_reference_to_likes.rb
@@ -0,0 +1,5 @@
+class AddUserReferenceToLikes < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :likes, :user
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_17_190520) do
+ActiveRecord::Schema.define(version: 2021_05_17_192816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +50,9 @@ ActiveRecord::Schema.define(version: 2021_05_17_190520) do
     t.bigint "likeable_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["likeable_type", "likeable_id"], name: "index_likes_on_likeable_type_and_likeable_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
